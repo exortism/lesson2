6b2b85182253e88935a0e2ed98b4ec563a8c113a
Exortism <exortism@live.ru>
2020-04-14 12:54:09 +0300

    task1

---
+++ lesson2_task1.py
@@ -0,0 +1,12 @@
+###########################################################################
+#TASK 1
+type_list = [int(1), str("python"), bool(None), float(0.35), {0, 1, 2, 3}]
+
+print(type_list)
+print(type(type_list[0]))
+print(type(type_list[1]))
+print(type(type_list[2]))
+print(type(type_list[3]))
+print(type(type_list[4]))
+
+###########################################################################
