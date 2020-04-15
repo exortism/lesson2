5022be332ca0ef7d26173456fc2056ec819eb396
exortism <exortism@live.ru>
2020-04-15 19:32:19 +0300

    task1 startingss`sdsd
    qweqwewith '#' will be ignored, and an empty message aborts the commit.

--- lesson2_task1.py
+++ lesson2_task1.py
@@ -3,6 +3,7 @@
 type_list = [int(1), str("python"), bool(None), float(0.35), {0, 1, 2, 3}]
 
 print(type_list)
+print(type(type_list))
 print(type(type_list[0]))
 print(type(type_list[1]))
 print(type(type_list[2]))
