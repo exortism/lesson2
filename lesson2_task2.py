9a82fd2f6af8e638a3bbaa10fafc2d00409c20be
exortism <exortism@live.ru>
2020-04-15 19:39:35 +0300

    last str

--- lesson2_task2.py
+++ lesson2_task2.py
@@ -12,3 +12,5 @@ my_list[0], my_list[1] = my_list[1], my_list[0]
 my_list[2], my_list[3] = my_list[3], my_list[2]
 print(my_list)
 ###########################################################################
+
+
