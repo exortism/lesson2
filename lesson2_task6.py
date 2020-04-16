5cecc67f87fb2d377fb8528d6e05f4133dd2226b
Exortism <exortism@live.ru>
2020-04-16 11:33:59 +0300

    TASK 6 FINAL VERSION

--- lesson2_task6.py
+++ lesson2_task6.py
@@ -6,7 +6,7 @@ while True:
         user_input_1 = input("Введите наименования товара, затем через ПРОБЕЛ его стоимость, количество,"
                              " и единицу измерения (шт, литров, см)").split()
         user_input_1 = (
-        user_input_1[0].split(), user_input_1[1].split(), user_input_1[2].split(), user_input_1[3].split(), )
+            user_input_1[0].split(), user_input_1[1].split(), user_input_1[2].split(), user_input_1[3].split(), )
         break
     except IndexError:
         print("Вы ошиблись")
@@ -15,7 +15,7 @@ while True:
         user_input_2 = input("Введите наименования ещё одного товара, затем через ПРОБЕЛ его стоимость, количество,"
                              " и единицу измерения (шт, литров, см)").split()
         user_input_2 = (
-        user_input_2[0].split(), user_input_2[1].split(), user_input_2[2].split(), user_input_2[3].split(), )
+            user_input_2[0].split(), user_input_2[1].split(), user_input_2[2].split(), user_input_2[3].split(), )
         break
     except IndexError:
         print("Вы ошиблись")
@@ -24,15 +24,18 @@ while True:
         user_input_3 = input("Введите наименования последнего товара, затем через ПРОБЕЛ его стоимость, количество,"
                              " и единицу измерения (шт, литров, см)").split()
         user_input_3 = (
-        user_input_3[0].split(), user_input_3[1].split(), user_input_3[2].split(), user_input_3[3].split(), )
+            user_input_3[0].split(), user_input_3[1].split(), user_input_3[2].split(), user_input_3[3].split(), )
         break
     except IndexError:
         print("Вы ошиблись")
-        break
 
 product_list1 = [(1, ), (user_input_1[0], user_input_1[1], user_input_1[2], user_input_1[3], )]
 product_list2 = [(2, ), (user_input_2[0], user_input_2[1], user_input_2[2], user_input_2[3], )]
 product_list3 = [(3, ), (user_input_3[0], user_input_3[1], user_input_3[2], user_input_3[3], )]
+
+print(product_list1)
+print(product_list2)
+print(product_list3)
 
 product_names = dict({"Название": [user_input_1[0], user_input_2[0], user_input_3[0]]})
 product_costs = dict({"Стоимость": [user_input_1[1], user_input_2[1], user_input_3[1]]})
@@ -45,6 +48,8 @@ result_dict = {
     "Количество": [user_input_1[2], user_input_2[2], user_input_3[2]],
     "Единица": [user_input_1[3], user_input_2[3], user_input_3[3]]
                }
+
+print(result_dict)
 
 ###################################################################################################################
 
