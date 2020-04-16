4a0201423ceb837d060cbf3ddbed1b5cfc9fdca3
Exortism <exortism@live.ru>
2020-04-16 12:21:17 +0300

    TASK 1

--- lesson2_task1.py
+++ lesson2_task1.py
@@ -1,7 +1,7 @@
 ###########################################################################
 # TASK 1
 
-type_list = [int(1), str("python"), bool(None), float(0.35), {0, 1, 2, 3}]
+type_list = [int(1), str("python"), bool(None), float(0.35), {0, 1, 2, 3}, tuple()]
 
 print(type_list)
 print(type(type_list))
@@ -10,6 +10,7 @@ print(type(type_list[1]))
 print(type(type_list[2]))
 print(type(type_list[3]))
 print(type(type_list[4]))
+print(type(type_list[6]))
 
 ###########################################################################
 
